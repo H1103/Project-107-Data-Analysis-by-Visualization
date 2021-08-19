d46940944f91d68aa467e9dab2bcc37a1ccd81a6
Hartej Nayar <71260380+H1103@users.noreply.github.com>
2021-08-19 23:50:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go 
 
 df = pd.read_csv("data.csv")
-# as_index = False means you indicate to groupby() that you don't want to set the column ID as the index
+# as_index = False is used to convert series of the data to dataframe
 mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 
 fig = px.scatter(mean, x = "student_id", y = "level", size = "attempt", color = "attempt")
